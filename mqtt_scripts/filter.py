a9d3a978fa62aed097b9719655120caf3f191134
Pablo <PabloCamp0s@users.noreply.github.com>
2023-10-30 14:14:29 +0100

    Import must be in first line

--- mqtt_scripts/filter.py
+++ mqtt_scripts/filter.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 import sys
 import time
-from __future__ import print_function
 
 for line in sys.stdin:
     if "##" in line:
